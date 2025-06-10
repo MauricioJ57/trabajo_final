31fb46c06bb5c272a6cacb4711a3b097ee548e61
@mauricio-jacki <mauriciojacki@gmail.com>
2025-06-09 23:19:59 -0300

    se borro el contenido del repositorio anterior

---
+++ scenes/gameplayjuego.js
@@ -0,0 +1,18 @@
+export default class gameplay extends Phaser.Scene {
+  constructor() {
+    super("gameplay");
+  }
+
+  init() {
+  }
+
+  preload() {
+  }
+
+  create() {
+  }
+
+  update() {
+  }
+
+}
